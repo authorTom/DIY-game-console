4ac01510b6bd053857f69605e04be2c2b9ed5313
Author Tom <AuthorTom@example.com>
2024-02-14 12:50:03 +0000

    Folders added

---
+++ Scripts/install_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update package lists to ensure you get the latest software versions
+sudo apt-get update
+
+# Install nano text editor
+sudo apt-get install nano -y
+
+echo "Nano installation completed successfully."
